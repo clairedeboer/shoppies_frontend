cb4ae0b504c52c0f5390a7d36e5cf97bd6e80b08
clairedeboer <claire.deboer@gmail.com>
2021-04-30 16:31:00 -0400

    search omd api on keystroke

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react'; 
 import './App.css';
 import MoviesPage from "./components/MoviesPage.js";
 import NominationsPage from "./components/NominationsPage.js"
 
 const App = () => {
+
+  const searchChange = (searchedWord) => {
+    fetch(`http://www.omdbapi.com/?t=${searchedWord}&apikey=8a111c74`)
+        .then((response) => response.json())
+        .then((omdData) => console.log(omdData));
+  }
+
   return (
     <div>
-      <MoviesPage />
+      <MoviesPage onSearchChange={searchChange}/>
       <NominationsPage />
       
     </div>
